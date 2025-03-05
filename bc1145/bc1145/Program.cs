4061d2154ceb29bdfe2a426c1a51eb6b56a0c378
David Dias <davidcostadias@gmail.com>
2025-03-05 14:18:15 -0300

    1145 resolvido

---
+++ bc1145/bc1145/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace bc1145
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] aux = Console.ReadLine().Split(' ');
+            int X = int.Parse(aux[0]);
+            int Y = int.Parse(aux[1]);
+            int cont = 1;
+
+            while (cont < Y)
+            {
+                for (int i = 1; i <= X; i++)
+                {
+                    Console.Write(cont);
+                    cont++;
+
+                    if (i == X)
+                        break;
+
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
