73b1bc7f5ba628cf32a4595576ccdfa3285770f7
David Dias <davidcostadias@gmail.com>
2025-03-05 16:26:40 -0300

    1149 resolvido

---
+++ bc1149/bc1149/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace bc1149
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] val = Console.ReadLine().Split(' ');
+            int A = int.Parse(val[0]);
+            int sum = 0;
+
+            int i = 1;
+            int N = int.Parse(val[i]);
+            while (N < 1)
+            {
+                i++;
+                N = int.Parse(val[i]);
+            }
+
+            for (i = 0; i < N; i++)
+            {
+                sum += A + i;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
