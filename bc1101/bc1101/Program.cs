74c8187c9a577fbccb8bb269e71281758def1759
David Dias <davidcostadias@gmail.com>
2025-03-05 12:38:51 -0300

    1101 resolvido

---
+++ bc1101/bc1101/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace bc1101
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+                string[] aux = Console.ReadLine().Split(' ');
+                int M = int.Parse(aux[0]);
+                int N = int.Parse(aux[1]);
+                int sum = 0;
+
+                if (M <= 0 || N <= 0)
+                    break;
+
+                for (int i = Math.Min(M, N); i <= Math.Max(M, N); i++)
+                {
+                    sum += i;
+                    Console.Write(i + " ");                   
+                }
+                Console.WriteLine("Sum=" + sum);
+            }
+        }
+    }
+}
