2becf82734b784550970d79b5df06090d04cf295
David Dias <davidcostadias@gmail.com>
2025-03-05 18:13:47 -0300

    1150 resolvido

---
+++ bc1150/bc1150/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace bc1150
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int X = int.Parse(Console.ReadLine());
+            int Z = int.Parse(Console.ReadLine());
+            
+            while (Z <= X)
+            {
+                Z = int.Parse(Console.ReadLine());
+            }
+
+            List<int> values = new List<int>();
+            
+            int sum = 0;
+            for (int i = 0; sum < Z; i++)
+            {
+                values.Add(X + i);
+                sum += values[i];
+            }
+
+            Console.WriteLine(values.Count);
+        }
+    }
+}
