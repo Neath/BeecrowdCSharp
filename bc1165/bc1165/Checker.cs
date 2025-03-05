ffcd1751cf09a3778310628a9dd5606cea86649d
David Dias <davidcostadias@gmail.com>
2025-03-05 16:08:41 -0300

    1165 resolvido

---
+++ bc1165/bc1165/Checker.cs
@@ -0,0 +1,15 @@
+﻿namespace bc1165
+{
+    static class Checker
+    {
+        static public bool PrimeCheck(int num)
+        {
+            for (int i = 2; i < (num - 1); i++)
+            {
+                if (num % i == 0)
+                    return false;
+            }
+            return true;
+        }
+    }
+}
--- bc1165/bc1165/Program.cs
+++ bc1165/bc1165/Program.cs
@@ -1,18 +1,10 @@
 ﻿using System;
+using static bc1165.Checker;
 
 namespace bc1165
 {
     class Program
     {
-        static bool PrimeCheck(int num)
-        {
-            for (int i = 2; i < (num - 1); i++)
-            {
-                if (num % i == 0)
-                    return false;
-            }
-            return true;
-        }
         static void Main(string[] args)
         {
             int N = int.Parse(Console.ReadLine());
@@ -20,9 +12,8 @@ namespace bc1165
             for (int i = 0; i < N; i++)
             {
                 int Num = int.Parse(Console.ReadLine());
-                bool IsPrime = PrimeCheck(Num);
 
-                if (IsPrime == true)
+                if (PrimeCheck(Num) == true)
                     Console.WriteLine(Num + " eh primo");
                 else
                     Console.WriteLine(Num + " nao eh primo");
