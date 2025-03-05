d7ff9fe0987f20ced0b36139242d0dfbe1a31784
David Dias <davidcostadias@gmail.com>
2025-03-05 11:37:14 -0300

    1049 resolvido

---
+++ bc1049/bc1049/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace bc1049
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string BigGroup = Console.ReadLine();
+            string MidGroup = Console.ReadLine();
+            string FeedingType = Console.ReadLine();
+            string answear;
+
+            if (BigGroup == "vertebrado")
+            {
+                if (MidGroup == "ave")
+                {
+                    if (FeedingType == "carnivoro")
+                        answear = "aguia";
+                    else
+                        answear = "pomba";
+                }
+                else 
+                {
+                    if (FeedingType == "onivoro")
+                        answear = "homem";
+                    else
+                        answear = "vaca";
+                }
+            }
+            else
+            {
+                if (MidGroup == "inseto")
+                {
+                    if (FeedingType == "hematofago")
+                        answear = "pulga";
+                    else
+                        answear = "lagarta";
+                }
+                else
+                {
+                    if (FeedingType == "hematofago")
+                        answear = "sanguessuga";
+                    else
+                        answear = "minhoca";
+                }
+            }
+
+            Console.WriteLine(answear);
+
+        }
+    }
+}
