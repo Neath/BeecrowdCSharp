e0773228fde67f4a8c0315cfbb3707fd634e0a64
David Dias <davidcostadias@gmail.com>
2025-03-05 15:57:43 -0300

    1165

---
+++ bc1165/bc1165/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace bc1165
+{
+    class Program
+    {
+        static bool PrimeCheck(int num)
+        {
+            for (int i = 2; i < (num - 1); i++)
+            {
+                if (num % i == 0)
+                    return false;
+            }
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < N; i++)
+            {
+                int Num = int.Parse(Console.ReadLine());
+                bool IsPrime = PrimeCheck(Num);
+
+                if (IsPrime == true)
+                    Console.WriteLine(Num + " eh primo");
+                else
+                    Console.WriteLine(Num + " nao eh primo");
+            }
+        }
+    }
+}
