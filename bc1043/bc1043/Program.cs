b513cca8b2b371ea1175cb648e4d42d72cee3b1a
David Dias <davidcostadias@gmail.com>
2025-03-08 15:09:19 -0300

    1043 resolvido

---
+++ bc1043/bc1043/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+
+namespace bc1043
+{
+    class Program
+    {
+        static bool IsTriangle(double a, double b, double c)
+        {
+            if (a + b > c && a + c > b && b + c > a)
+                return true;
+            else
+                return false;
+        }
+
+        static double Area(double a, double b, double c)
+        {
+            return (((a + b) * c) / 2.0);
+        }
+
+        static double Perimeter(double a, double b, double c)
+        {
+            return (a + b + c);
+
+        }
+
+        static void Main(string[] args)
+        {
+            string[] aux = Console.ReadLine().Split(' ');
+            double A = double.Parse(aux[0], CultureInfo.InvariantCulture);
+            double B = double.Parse(aux[1], CultureInfo.InvariantCulture);
+            double C = double.Parse(aux[2], CultureInfo.InvariantCulture);
+            if (IsTriangle(A, B, C))
+            {
+                Console.WriteLine("Perimetro = " + Perimeter(A, B, C).ToString("F1", CultureInfo.InvariantCulture));
+            }
+            else
+            {
+                Console.WriteLine("Area = " + Area(A, B, C).ToString("F1", CultureInfo.InvariantCulture));
+            }
+        }
+    }
+}
---
+++ bc1043/bc1043/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("bc1043")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("bc1043")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f15e78fd-8354-45c2-affd-b9b86e72db59")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
