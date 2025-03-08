f0e6690955024954c6735b5d9a2fbf3ad570ee05
David Dias <davidcostadias@gmail.com>
2025-03-08 12:29:31 -0300

    1142 resolvido

---
+++ bc1142/bc1142/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace bc1142
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int num = 1;
+
+            for (int i = 1; i <= N; i++)
+            {
+                Console.WriteLine($"{num} {num+1} {num+2} PUM");
+                num += 4;
+            }
+        }
+    }
+}
