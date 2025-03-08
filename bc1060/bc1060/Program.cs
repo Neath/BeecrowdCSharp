d7858a1f303aa6f815a085e1cd4e047bb178f072
David Dias <davidcostadias@gmail.com>
2025-03-08 15:26:33 -0300

    1060 resolvido

---
+++ bc1060/bc1060/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace bc1060
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int QtdPos = 0;
+            for (int i = 0; i < 6; i++)
+            {
+                double num = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                if (num > 0.0)
+                    QtdPos++;
+            }
+            Console.WriteLine(QtdPos + " valores positivos");
+        }
+    }
+}
