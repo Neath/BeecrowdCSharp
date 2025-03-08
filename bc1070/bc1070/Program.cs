a74350e62633d4fa241f93d9ecbea4c2fe8dfc3d
David Dias <davidcostadias@gmail.com>
2025-03-08 15:46:16 -0300

    1070 resolvido

---
+++ bc1070/bc1070/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace bc1070
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int cont = 0;
+
+            while (cont < 6)
+            {
+                if (num % 2 != 0)
+                {
+                    Console.WriteLine(num);
+                    cont++;
+                }
+
+                num++;
+            }
+        }
+    }
+}
