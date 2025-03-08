aa6a15a544060ba76bff9541e499205435515f48
David Dias <davidcostadias@gmail.com>
2025-03-08 14:08:09 -0300

    1155 resolvido

---
+++ bc1155/bc1155/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace bc1155
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Soma = 0.0;
+
+            for (int i = 1; i <= 100; i++)
+            {
+                Soma += 1.0 / Convert.ToDouble(i);
+            }
+            Console.WriteLine(Soma.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
