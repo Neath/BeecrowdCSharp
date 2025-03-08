2c60c65b157d9b40c2dab0cd664817af1fadda04
David Dias <davidcostadias@gmail.com>
2025-03-08 13:59:55 -0300

    1153 resolvido

---
+++ bc1153/bc1153/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace bc1153
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int aux = N;
+
+            for (int i = 1; i < aux; i++)
+            {
+                N = N*(aux - i);
+            }
+            Console.WriteLine(N);
+        }
+    }
+}
