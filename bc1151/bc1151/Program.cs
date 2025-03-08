e8d04ae52547d4f7f9db626cd37bff713992c41b
David Dias <davidcostadias@gmail.com>
2025-03-08 13:13:18 -0300

    1151 resolvido

---
+++ bc1151/bc1151/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace bc1151
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int primeiro = 0;
+            int segundo = 1;
+            int result = primeiro + segundo;
+
+            if (N == 1)
+            {
+                Console.Write(primeiro);
+            }
+            else if (N == 2)
+            {
+                Console.Write($"{primeiro} {segundo}");
+            }
+            else
+            {
+                Console.Write($"{primeiro} {segundo}");
+                for (int i = 2; i < N; i++)
+                {
+                    result = primeiro + segundo;
+                    primeiro = segundo;
+                    segundo = result;
+                    Console.Write(" " + result);
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
