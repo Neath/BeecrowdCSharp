3b66b5dc1b6a02e1e668957aa4ed29e0dac12a02
David Dias <davidcostadias@gmail.com>
2025-03-08 15:37:37 -0300

    1066 resolvido

--- bc1065/bc1065/Program.cs
+++ bc1065/bc1065/Program.cs
@@ -7,14 +7,29 @@ namespace bc1065
         static void Main(string[] args)
         {
             int QtdPar = 0;
+            int QtdImpar = 0;
+            int QtdPos = 0;
+            int QtdNeg = 0;
+
             for (int i = 0; i < 5; i++)
             {
                 int num = int.Parse(Console.ReadLine());
-                if (num % 2 == 0)              
+
+                if (num % 2 == 0)
                     QtdPar++;
-                
+                else
+                    QtdImpar++;
+
+                if (num > 0)
+                    QtdPos++;
+                else if (num < 0)
+                    QtdNeg++;
+
             }
-            Console.WriteLine(QtdPar + " valores pares");
+            Console.WriteLine(QtdPar + " valor(es) par(es)");
+            Console.WriteLine(QtdImpar + " valor(es) impar(es)");
+            Console.WriteLine(QtdPos + " valor(es) positivo(s)");
+            Console.WriteLine(QtdNeg + " valor(es) negativo(s)");
         }
     }
 }
