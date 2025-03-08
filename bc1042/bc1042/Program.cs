af9e635c60b9abf7a7d496c1176f340a247cc30f
David Dias <davidcostadias@gmail.com>
2025-03-08 14:56:10 -0300

    1042 resolvido

---
+++ bc1042/bc1042/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace bc1042
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] aux = Console.ReadLine().Split(' ');
+            int[] values = new int[3];
+            List<int> Sorted = new List<int>();
+
+            for (int i = 0; i < 3; i++)
+                values[i] = int.Parse(aux[i]);
+
+            for (int i = 0; i < 3; i++)
+                Sorted.Add(values[i]);
+
+            Sorted.Sort();
+
+            foreach (int num in Sorted)
+            {
+                Console.WriteLine(num);
+            }
+            Console.WriteLine();
+            foreach (int num in values)
+            {
+                Console.WriteLine(num);
+            }          
+        }
+    }
+}
