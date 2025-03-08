884245714e46a1dd31659cb45f5592ecb8f71b1e
David Dias <davidcostadias@gmail.com>
2025-03-08 14:18:44 -0300

    1156 resolvido

---
+++ bc1156/bc1156/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace bc1156
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Soma = 0.0;
+
+            for (int i = 1, j = 1; i < 40; i += 2, j *= 2)
+            {
+                Soma += (Convert.ToDouble(i) / Convert.ToDouble(j));
+            }
+            Console.WriteLine(Soma.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
