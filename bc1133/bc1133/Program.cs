60d161168215ec7c887acb6e8e9a0c8c849baeac
David Dias <davidcostadias@gmail.com>
2025-03-08 12:21:07 -0300

    1133 resolvido

---
+++ bc1133/bc1133/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace bc1133
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int X = int.Parse(Console.ReadLine());
+            int Y = int.Parse(Console.ReadLine());
+            int Min = Math.Min(X, Y);
+            int Max = Math.Max(X, Y);
+
+            for (int i = Min+1; i < Max; i++)
+            {
+                if (i % 5 == 2 || i % 5 == 3)
+                    Console.WriteLine(i);
+            }
+        }
+    }
+}
