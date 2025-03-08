ce47d81260a31e085b77e321dd8f2569268a12f5
David Dias <davidcostadias@gmail.com>
2025-03-08 11:09:50 -0300

    1096 resolvido

---
+++ bc1096/bc1096/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace bc1096
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 10; i += 2)
+            {
+                for (int j = 7; j > 4; j--)
+                {
+                    Console.WriteLine($"I={i} J={j}");
+                }
+            }
+        }
+    }
+}
