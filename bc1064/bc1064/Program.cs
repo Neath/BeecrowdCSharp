a98633bb4d880347d6c9fd5cec0a11eb1bcc3b52
David Dias <davidcostadias@gmail.com>
2025-03-08 15:29:59 -0300

    1064 resolvido

---
+++ bc1064/bc1064/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace bc1064
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int QtdPos = 0;
+            double SomaPos = 0.0;
+            for (int i = 0; i < 6; i++)
+            {
+                double num = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                if (num > 0.0)
+                {
+                    SomaPos += num;
+                    QtdPos++;
+                }
+            }
+            double MediaPos = (double)(SomaPos / QtdPos);
+            Console.WriteLine(QtdPos + " valores positivos");
+            Console.WriteLine(MediaPos.ToString("F1", CultureInfo.InvariantCulture));
+        }
+    }
+}
