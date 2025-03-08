71d0bf20a838429df478bce455f0140d166926ad
David Dias <davidcostadias@gmail.com>
2025-03-08 15:32:45 -0300

    1065 resolvido

---
+++ bc1065/bc1065/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace bc1065
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int QtdPar = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                if (num % 2 == 0)              
+                    QtdPar++;
+                
+            }
+            Console.WriteLine(QtdPar + " valores pares");
+        }
+    }
+}
