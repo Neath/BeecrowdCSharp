56b8c6655efafe27d46f43efb30e082bd0b286ee
David Dias <davidcostadias@gmail.com>
2025-03-08 11:15:52 -0300

    1097 resolvido

---
+++ bc1097/bc1097/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace bc1097
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int aux = 7;
+            for (int i = 1; i < 10; i += 2)
+            {
+                for (int j = aux; j > aux-3; j--)
+                {
+                    Console.WriteLine($"I={i} J={j}");                    
+                }
+                aux += 2;
+            }
+        }
+    }
+}
